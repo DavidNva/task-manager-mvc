f8a8b5bb92a9c1b97176eeefcde590669dc73fa1
David Nava <david.nava.garcia4@gmail.com>
2025-10-19 13:47:50 -0600

    Añadir registro de usuarios y personalización de errores
    Se han implementado nuevas funcionalidades para el registro de usuarios, incluyendo un controlador específico (`UsuariosController.cs`) y un modelo de vista (`RegistroViewModel.cs`). Se ha creado un formulario de registro (`Registro.cshtml`) con validaciones y mensajes de error en español, gracias a la nueva clase `MensajesDeErrorIdentity.cs` que personaliza los mensajes de error de identidad. Además, se ha establecido una política de autorización global en `Program.cs` y se ha modificado el layout en `_Layout.cshtml` para adaptarse a las vistas de usuario.

---
+++ TaskManagerMVC/Controllers/UsuariosController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TaskManagerMVC.Models;
+
+namespace TaskManagerMVC.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public UsuariosController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Registro(RegistroViewModel modelo)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(modelo);
+            }
+            var usuario = new IdentityUser { Email = modelo.Email, UserName = modelo.Email };
+
+            var resultado = await _userManager.CreateAsync(usuario, modelo.Password);
+
+            if (resultado.Succeeded)
+            {
+                await _signInManager.SignInAsync(usuario, isPersistent: true);//Mantener la sesión iniciada
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                foreach(var error in resultado.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);//Esto agrega los errores al modelo para mostrarlos en la vista, que serian tipo asp-validation-summary
+                }
+                return View(modelo);
+            }
+        }
+    }
+}
--- TaskManagerMVC/Program.cs
+++ TaskManagerMVC/Program.cs
@@ -1,19 +1,37 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using TaskManagerMVC;
+using TaskManagerMVC.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//Vamos a crear un filtro global para que todas las paginas requieran autenticacion
+var politicaUsuariosAutenticados = new AuthorizationPolicyBuilder()
+    .RequireAuthenticatedUser()
+    .Build();
+
+
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews(opciones =>
+{
+    opciones.Filters.Add(new AuthorizeFilter(politicaUsuariosAutenticados));
+});//Agregamos el filtro global a todas las paginas para que requieran autenticacion, a excepcion de las que tengan el atributo [AllowAnonymous]
+
 builder.Services.AddDbContext<ApplicationDBContext>(opciones => opciones.UseSqlServer("name=DefaultConnection"));
 builder.Services.AddAuthentication();
 builder.Services.AddIdentity<IdentityUser, IdentityRole>(opciones =>
 {
     opciones.SignIn.RequireConfirmedAccount = false;//Para que no pida confirmacion de cuenta al registrar un nuevo usuario.
 }).AddEntityFrameworkStores<ApplicationDBContext>()
-.AddDefaultTokenProviders();//Esto es para que use los tokens por defecto para cosas como resetear contraseñas, etc. y el application db context que creamos antes es el que usara para almacenar los usuarios y roles.
+.AddDefaultTokenProviders()
+.AddErrorDescriber<MensajesDeErrorIdentity>();
+//AddDefaultTokenProviders:Esto es para que use los tokens por defecto para cosas como resetear contraseñas, etc. y el ApplicationDBContext: application db context que creamos antes es el que usara para almacenar los usuarios y roles.
+
+//Para agregar mensajes en español en las validaciones de identidad, que tenemos la clase MensajesDeErrorIdentity en Models, lo que haremos es agregar un servicio que use esa clase como proveedor de mensajes de error de identity core
+
 
 builder.Services.PostConfigure<CookieAuthenticationOptions>(IdentityConstants.ApplicationScheme, opciones =>
 {
