7274459e5146d5438f2ac697390f4733ddee4cad
David Nava <david.nava.garcia4@gmail.com>
2025-11-02 22:47:59 -0600

    Agregando tareas usando knockout
    Primera parte agregando tareas, aun no se guarda en la bd, ni se retorna de la bd. Hemos trabajado hasta ahora con las tareas en memoria

--- TaskManagerMVC/Migrations/20251022132845_AdminRol.cs
+++ TaskManagerMVC/Migrations/20251022132845_AdminRol.cs
@@ -23,7 +23,7 @@ namespace TaskManagerMVC.Migrations
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql(@"DELETE FROM AspNetRoles WHERE Id = '5b4b23a2-193e-4f14-bb6e-05b17f8d4a77'");
-            //usare delete o delete from realmente es lo mismo en este caso, pero delete from es mas comun. La diferencia es que delete from se usa mas en sql puro y delete en linq. es recomienda usar delete from en sql puro porque es mas claro, aunque realmente no hay diferencia en usar uno u otro.
+            //usare delete o delete from realmente es lo mismo en este caso, pero delete from es mas comun. La diferencia es que delete from se usa mas en sql puro y delete en linq. es recomienda usar delete from en sql puro porque es mas claro, aunque realmente no hay diferencia en usar uno u otro. Ahora, por lo eliminamos: esto debido a que si revertimos la migracion, queremos asegurarnos de que el rol admin ya no exista en la base de datos.
         }
     }
 }
---
+++ TaskManagerMVC/wwwroot/js/Tareas.js
@@ -0,0 +1,17 @@
+﻿function agregarNuevaTareaAlListado() {
+    tareaListadoViewModel.tareas.push(new TareaElementoListadoViewModel({ id: 0, titulo: '' }));
+
+    $("[name=titulo-tarea]").last().focus();
+}
+
+
+function manegarFocusOutTituloTarea(tarea) {
+    const titulo = tarea.titulo();
+
+    if (!titulo) {
+        tareaListadoViewModel.tareas.pop();//Eliminamos el elementos del final
+        return;
+    }
+
+    tarea.id(1);
+}
